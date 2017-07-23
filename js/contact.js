3792620b6dd51618f5dcbe4078d1b9f39a838dbe
cubeboy05 <cubeboy05@gmail.com>
2017-07-23 17:23:28 +0100

    form setup, social media added.

---
+++ js/contact.js
@@ -0,0 +1,36 @@
+
+$(function () {
+    $('form#formid').on('submit', function (e) {
+      e.preventDefault();
+      $.ajax({
+        type: 'post',
+        url: '../mail/contact_me.php',
+        data: $('form#formid').serialize(),
+        success: function () {
+
+          $("#sendmessagebtm").show().delay(4000).fadeOut();
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+
+          $("#errormessagebtm").show().delay(4000).fadeOut();
+        }
+      });
+    });
+
+    $('form#contact').on('submit', function (e) {
+      e.preventDefault();
+      $.ajax({
+        type: 'post',
+        url: '../mail/contact_me.php',
+        data: $('form#contact').serialize(),
+        success: function () {
+
+          $("#sendmessagetop").show().delay(4000).fadeOut();
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            
+          $("#errormessagetop").show().delay(4000).fadeOut();
+        }
+      });
+    });
+});
