aee2dba48e68c4fa98efd8703aa22174fac994b4
cubeboy05 <cubeboy05@gmail.com>
2017-07-30 15:25:32 +0100

    amended form inputs

--- js/contact.js
+++ js/contact.js
@@ -28,8 +28,25 @@ $(function () {
           $("#sendmessagetop").show().delay(4000).fadeOut();
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
-            
+
           $("#errormessagetop").show().delay(4000).fadeOut();
+        }
+      });
+    });
+
+    $('form#contact1').on('submit', function (e) {
+      e.preventDefault();
+      $.ajax({
+        type: 'post',
+        url: '../mail/contact_me.php',
+        data: $('form#contact1').serialize(),
+        success: function () {
+
+          $("#sendmessagetop1").show().delay(4000).fadeOut();
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+
+          $("#errormessagetop1").show().delay(4000).fadeOut();
         }
       });
     });
